6ddf8aa08b7de4ce3b0b80311ec00e7c9c2aeaee
suraj2493 <51969235+suraj2493@users.noreply.github.com>
2020-09-03 13:05:30 +0530

    Update Insert.java

--- DataStructure/src/com/suraj/Node/Insert.java
+++ DataStructure/src/com/suraj/Node/Insert.java
@@ -4,9 +4,6 @@ public class Insert{
 public static void main(String[] args){
 
 int[] arr= {1,6,3,8,9,100,1888};
-  for(i=0;i<arr.length;i++){
-   System.out.println(arr[i]);
-  }
 int z=6;
 Java j= new Java();
 j.findNumberIndex(arr,z);
