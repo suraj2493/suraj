24389e73530b2b604a779171520cef8c860cb6e5
suraj2493 <51969235+suraj2493@users.noreply.github.com>
2020-09-04 08:50:06 +0530

    changes in node.java file

--- DataStructure/src/com/suraj/Node/Node.java
+++ DataStructure/src/com/suraj/Node/Node.java
@@ -4,15 +4,4 @@ public class Node {
 int data;
 Node next;
 Node previous;
- private void syso() {
-	System.out.println("sss");
-System.out.println("sss");
-	 System.out.println("sss1");
-	 System.out.println("sss3");
-System.out.println("sss");
-System.out.println("ssssmy changes");
-System.out.println("pull request");
-
-
-}
 }
