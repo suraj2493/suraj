a0092079c83bc1dc94b45a8a7e9a960d1ce4a9c9
suraj2493 <kumarsuraj2493@gmail.com>
2020-09-18 21:09:23 +0530

    commit 4

--- DataStructure/src/com/suraj/Node/Cap.java
+++ DataStructure/src/com/suraj/Node/Cap.java
@@ -6,7 +6,8 @@ public class Cap {
        
         System.out.println("suraj");
         System.out.println("shanu");
-	System.out.println("kiran");
+	    System.out.println("kiran");
+	    System.out.println("shambhu");
 	}
 
 }
